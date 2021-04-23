0902eb6c23e6f5d550d009f92d574b8dacb53ce2
AdvikGhorpadkar <70799562+AdvikGhorpadkar@users.noreply.github.com>
2021-04-23 19:51:29 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,28 @@
+class Box {
+    constructor (x,y,width,height) {
+        var options = {
+            restitution:0.4,
+            friction:1,
+            //isStatic:true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.visiblity =255;
+        World.add(world, this.body);
+    }
+
+display() {
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rectMode(CENTER);
+        strokeWeight(2);
+        stroke(0);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+
+
+}
